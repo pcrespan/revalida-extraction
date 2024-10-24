2a8bf152f398d31706b1e76c426fa3381e94547b
pcrespan <pedrocrespan@hotmail.com>
2024-10-24 16:06:26 -0300

    Criando função para converter questões para JSON. Subindo provas parcialmente normalizadas

---
+++ extractor/convert_txt_to_json.py
@@ -0,0 +1,49 @@
+import json
+
+
+def processar_arquivo_texto(nome_arquivo):
+    questoes = []
+    with open(nome_arquivo, 'r', encoding='utf-8') as file:
+        questao = {}
+        alternativas = {}
+
+        for linha in file:
+            linha = linha.strip()
+
+            if linha.startswith("QUESTÃO") or linha.startswith("Questão") or linha.startswith("questão"):
+                if questao:
+                    questao['alternativas'] = alternativas
+                    questoes.append(questao)
+                    questao = {}
+                    alternativas = {}
+                numero_questao = int(linha.split()[1])
+                questao['numero'] = numero_questao
+
+            elif linha and (linha[0] in "ABCDE"):
+                letra, texto_alternativa = linha[0], linha[2:].strip()
+                alternativas[letra] = texto_alternativa
+
+            elif linha:
+                if 'enunciado' not in questao:
+                    questao['enunciado'] = linha
+                else:
+                    questao['enunciado'] += " " + linha
+        if questao:
+            questao['alternativas'] = alternativas
+            questoes.append(questao)
+
+    return questoes
+
+
+def salvar_json(questoes, nome_arquivo_json):
+    with open(nome_arquivo_json, 'w', encoding='utf-8') as file:
+        json.dump({"questoes": questoes}, file, ensure_ascii=False, indent=4)
+
+tests = [2011, 2012, 2013, 2014, 2015, 2016]
+
+for test in tests:
+    arquivo_texto = "tests/{}.txt".format(test)
+    arquivo_json = "questoes{}.json".format(test)
+    questoes_processadas = processar_arquivo_texto(arquivo_texto)
+    salvar_json(questoes_processadas, arquivo_json)
+    print(f"Questões processadas e salvas em {arquivo_json}")
